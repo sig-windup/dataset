e100e1e198b9f8b8dccbe21d9fb9357e070db8a3
박유경 <51533341+codename-602@users.noreply.github.com>
2021-07-08 18:24:56 +0900

    객체 배열화 완료
    CSV 본문 전처리 수정해야 함
    문제점 1. 지 멋대로 개행 됨 2. ','가 들어가면 쪼개짐

--- crawling.py
+++ crawling.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from datetime import date
+import csv
 
 TEAM_NAME = dict(NC='NC', OB='두산', KT='KT', LG='LG', WO='키움', HT='KIA', LT='롯데', SS='삼성', SK='SSG', HH='한화')
 # selenium에서 사용할 웹 드라이버 절대 경로 정보
@@ -9,6 +10,42 @@ driver = webdriver.Chrome(chromedriver)
 # driver로 특정 페이지를 크롤링한다.
 URL = 'https://sports.news.naver.com/kbaseball/news/index?isphoto=N&type=team&team='
 driver.get(URL)
+
+
+class Data:
+    def __init__(self, team, date, image, content):
+        self.date = date
+        self.team = team
+        self.image = image
+        self.content = content
+
+    def getDate(self):
+        return self.date
+
+    def getTeam(self):
+        return self.team
+
+    def getImage(self):
+        return self.image
+
+    def getContent(self):
+        return self.content
+
+    def setDate(self, date):
+        self.date = date
+
+    def setTeam(self, team):
+        self.team = team
+
+    def setImage(self, image):
+        self.image = image
+
+    def setContent(self, content):
+        self.content = content
+
+    def __str__(self):
+        return '구단: ' + self.team + ' 날짜: ' + self.date + ' 사진: ' + self.image + ' 내용: ' + self.content
+
 
 # 데이터 가져오기(getTeams(): 구단, get5Days(): 최근 5일, getArticles(): 기사의 주소)
 def getTeams():
@@ -43,11 +80,12 @@ def getContents(address):
     content = driver.find_element_by_id('newsEndContents')
     imageElement = driver.find_element_by_xpath('//span[@class=\'end_photo_org\']')
     imageURL = imageElement.find_element_by_tag_name('img').get_attribute('src')
-    return title.text + str(content.text).split('기사')[0], imageURL
+    return title.text + str(str(content.text).split('기사')[0]), imageURL
 
 
 # team 코드로 최근 5일 기사 크롤링
 def start_crawling(team_code):
+    data = []
     team_name = TEAM_NAME[team_code]
     days = get5Days()
     for d in days:
@@ -56,14 +94,14 @@ def start_crawling(team_code):
         address = getAddress()
         for a in address:
             content, image = getContents(a)
-            print("구단: ", team_name)
-            print("날짜: ", d)
-            print("사진: ", image)
-            print("내용: ", content)
+            article = Data(team_name, d, image, content)
+            data.append(article)
+    return data
 
 
 # 전 구단 오늘 기사 크롤링
 def today_crawling(URL):
+    data = []
     teams = getTeams()
     for t in teams:
         URL += t
@@ -71,44 +109,11 @@ def today_crawling(URL):
         address = getAddress()
         for a in address:
             content, image = getContents(a)
-            print("구단: ", TEAM_NAME[t])
-            print("날짜: ", date.today().strftime('%Y%m%d'))
-            print("사진: ", image)
-            print("내용: ", content)
+            article = Data(TEAM_NAME[t], date.today().strftime('%Y%m%d'), image, content)
+            data.append(article)
         URL = 'https://sports.news.naver.com/kbaseball/news/index?isphoto=N&type=team&team='
+    return data
 
-        
+
 # start_crawling(teams[6])
-today_crawling(URL)
-
-
-class Data:
-    def __init__(self, date, team, image, content):
-        self.date = date
-        self.team = team
-        self.image = image
-        self.content = content
-
-    def getDate(self):
-        return self.date
-
-    def getTeam(self):
-        return self.team
-
-    def getImage(self):
-        return self.image
-
-    def getContent(self):
-        return self.content
-
-    def setDate(self, date):
-        self.date = date
-
-    def setTeam(self, team):
-        self.team = team
-
-    def setImage(self, image):
-        self.image = image
-
-    def setContent(self, content):
-        self.content = content
+# today_crawling(URL)
